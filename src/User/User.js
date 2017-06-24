501b25d501bd0bdf9b88e4f7883528e58ab46fc4
Aravind Reddy V <zerodividedby0@gmail.com>
2017-06-24 15:32:08 +0530

    Added name setters and getters

--- src/User/User.js
+++ src/User/User.js
@@ -2,8 +2,55 @@ const config = require('config');
 // const debug = require('debug')('User');
 const Auth = require('../Auth/Auth');
 
-class User {
+class Person {
+    setFirstName(firstName) {
+        this.firstName = firstName;
+    }
+
+    getFirstName() {
+        return this.firstName;
+    }
+
+    setMiddleName(middleName) {
+        this.middleName = middleName;
+    }
+
+    getMiddleName() {
+        return this.middleName;
+    }
+
+    setLastName(lastname) {
+        this.lastname = lastname;
+    }
+
+    getLastName() {
+        return this.lastname;
+    }
+
+    setShortName(shortname) {
+        this.shortname = shortname;
+    }
+
+    getShortName() {
+        return this.shortname;
+    }
+
+    setFullName(fullname) {
+        if (fullname) {
+            this.fullname = fullname;
+            return true;
+        }
+        return false;
+    }
+
+    getFullName() {
+        return this.fullname;
+    }
+}
+
+class User extends Person{
     constructor(username, password) {
+        super();
         this.username = username;
         this.password = password;
     }
